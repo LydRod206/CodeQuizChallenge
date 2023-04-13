13c8e8bb976803a64db36b04bd92e8360f4c4d16
LydRod206 <119384486+LydRod206@users.noreply.github.com>
2023-04-13 13:25:01 -0700

    file structure, html, reset css, some pseudocode

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+// Define quiz questions and answers
+  // Define variables
+ // Start the quiz
+  // Display a question
+  // Select an answer
+  // Start the timer
+  // End the quiz
+  // Save score and initials
